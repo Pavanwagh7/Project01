3f3abd912499d4f0df5eb7669e4cc9153db177d5
Pavan Wagh <pawan.s.wagh@gmail.com>
2026-01-22 20:59:14 +0530

    CBS-V1 Main.java commited

---
+++ src/Bank/Main.java
@@ -0,0 +1,11 @@
+package Bank;
+
+
+public class Main {
+    public static void main(String[] args) {
+        //this is main class with main method
+        Dashboard d = new Dashboard();
+        d.menu();
+    }
+}
+
